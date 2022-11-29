7e797f3a62d1b72c02535a4b461195d8a70ff9d8
Azim Kubatov <kubat.azim@gmail.com>
2022-11-29 16:55:23 +0600

    handle internet connection

--- lib/presentation/archive/archive_page.dart
+++ lib/presentation/archive/archive_page.dart
@@ -1,12 +1,16 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:ok_radio_flutter/core/di/inject.dart';
 import 'package:ok_radio_flutter/core/navigation/router.gr.dart';
+import 'package:ok_radio_flutter/core/newtork/network_checker.dart';
 import 'package:ok_radio_flutter/util/colors.dart';
 import '../../core/navigation/navigation.dart';
 import 'cubit/archive_cubit.dart';
 import 'cubit/archive_state.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ArchivePage extends StatelessWidget {
   ArchivePage({Key? key}) : super(key: key);
@@ -31,51 +35,59 @@ class ArchivePage extends StatelessWidget {
             shrinkWrap: true,
             itemCount: state.programs.length,
             itemBuilder: (context, index) => InkWell(
-              onTap: () => Navigation.router.push(
-                ArchiveDetailsRoute(
-                  id: state.programs[index].id!,
-                  title: state.programs[index].title!,
-                  index: index,
+              onTap: () async {
+                if (!await NetworkChecker().isConnected) {
+                  Fluttertoast.showToast(
+                      msg: AppLocalizations.of(context)!.noConnection);
+                  return;
+                }
+                Navigation.router.push(
+                  ArchiveDetailsRoute(
+                    id: state.programs[index].id!,
+                    title: state.programs[index].title!,
+                    index: index,
+                  ),
+                );
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(vertical: 8),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.black12,
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CachedNetworkImage(
+                      imageUrl: state.programs[index].image ?? '',
+                      width: 100,
+                      placeholder: (context, url) =>
+                          Center(child: CircularProgressIndicator()),
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text(
+                            state.programs[index].title!,
+                            style: TextStyle(
+                              color: AppColors.primary,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(state.programs[index].content!),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
-              child: Container(
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(
-                      width: 1,
-                      color: Colors.black12,
-                    ),
-                  ),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CachedNetworkImage(
-                        imageUrl: state.programs[index].image ?? '',
-                        width: 100,
-                        placeholder: (context, url) =>
-                            Center(child: CircularProgressIndicator()),
-                        errorWidget: (context, url, error) => Icon(Icons.error),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Text(
-                              state.programs[index].title!,
-                              style: TextStyle(
-                                color: AppColors.primary,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(state.programs[index].content!),
-                          ],
-                        ),
-                      )
-                    ],
-                  )),
             ),
           ),
         );
